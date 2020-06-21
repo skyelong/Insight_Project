5191dc04445631192c3067fdc1852270d884a11e
Skye Long <skye.m.long@gmail.com>
2020-06-21 12:17:45 -0400

    Update a_Model.py

--- happy_trees/a_Model.py
+++ happy_trees/a_Model.py
@@ -24,9 +24,9 @@ import hsv_shift as hsv
 
 
 def ModelIt(fromUser  = 'Default'):
-    hsv_knn_chroma = load('/home/ubuntu/Insight_project/models/ds_h_chroma.joblib')
-    hsv_knn_neutral = load('/home/ubuntu/Insight_project/models/ds_h_neutrals.joblib')
-    pixels = hsv.import_convert_pixelize("/home/ubuntu/Insight_project/happy_trees/static/image2.jpg")
+    hsv_knn_chroma = load('/home/ubuntu/Insight_Project/models/ds_h_chroma.joblib')
+    hsv_knn_neutral = load('/home/ubuntu/Insight_Project/models/ds_h_neutrals.joblib')
+    pixels = hsv.import_convert_pixelize('/home/ubuntu/Insight_Project/happy_trees/static/image2.jpg')
     
     shifted_colors, shifted_neutrals = hsv.shift_h_split(pixels, .25, .25)
     
